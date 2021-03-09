6c08c713bf25aa28b3345777bfeaccf66d477407
Michael Ducharm <ducharmdev@gmail.com>
2021-03-08 22:11:28 -0500

    added script to populate data into template

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using static PlotNet.GraphGenerator;
+
+
+var path = args[0] ?? Directory.GetCurrentDirectory();
+
+var repoName = new DirectoryInfo(path).Name; 
+
+var data = GenerateGraphData(path);
+var serializedData = JsonSerializer.Serialize(data);
+
+var generatedGraph = File.ReadAllText("template.html")
+    .Replace("var data = [];", $"var data = {serializedData};");
+
+File.WriteAllText($"{repoName}.Graph.html", generatedGraph);
