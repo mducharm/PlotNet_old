65f8ccaa832ef1ab1b7f3310946c2bfb2547977a
Michael Ducharm <ducharmdev@gmail.com>
2021-03-12 17:22:35 -0500

    moved console app into separate project

--- PlotNet.Console/Program.cs
+++ PlotNet.Console/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Text.Json;
 using static PlotNet.GraphGenerator;
 
-
 var path = args[0] ?? Directory.GetCurrentDirectory();
 
 var repoName = new DirectoryInfo(path).Name; 
@@ -17,7 +16,6 @@ var options = new JsonSerializerOptions
 
 var serializedData = JsonSerializer.Serialize(data, options);
 
-var generatedGraph = File.ReadAllText("template.html")
-    .Replace("<!-- data -->", $"{serializedData}");
+var generatedGraph = AdditionalFiles.AsConstants.templateContents.Replace("<!-- data -->", $"{serializedData}");
 
 File.WriteAllText($"{repoName}.Graph.html", generatedGraph);
