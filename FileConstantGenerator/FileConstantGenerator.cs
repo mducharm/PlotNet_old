a2af656fee2455c248c991f57bd893047277fef3
Michael Ducharm <ducharmdev@gmail.com>
2021-03-12 18:58:52 -0500

    cleaned up helper methods

--- FileConstantGenerator/FileConstantGenerator.cs
+++ FileConstantGenerator/FileConstantGenerator.cs
@@ -22,7 +22,7 @@ public class FileConstantGenerator : ISourceGenerator
     {
         var constants = GetFiles(context)
                         .Select(file => file.ToString())
-                                .Aggregate("", (a, c) => $"{a}{c}\n");
+                        .Aggregate("", (a, c) => $"{a}{c}\n");
 
         var source = @"
 using System;
@@ -30,7 +30,7 @@ namespace AdditionalFiles
 {
     public class AsConstants
     {
-        " 
+        "
     + constants +
     @"}
 }";
@@ -38,18 +38,16 @@ namespace AdditionalFiles
         context.AddSource("Template.cs", SourceText.From(source, Encoding.UTF8));
     }
 
-    
-    static IEnumerable<FileMetadata> GetFiles(GeneratorExecutionContext context)
-    {
-        return context.AdditionalFiles.Select((AdditionalText f) => {
-            return new FileMetadata
-            {
-                Name = Path.GetFileNameWithoutExtension(f.Path),
-                Extension = Path.GetExtension(f.Path),
-                Contents = f.GetText().ToString()
-            };
-        });
 
-    }
+    static IEnumerable<FileMetadata> GetFiles(GeneratorExecutionContext context) =>
+        context.AdditionalFiles.Select(MapToFileMetadata);
+
+    static FileMetadata MapToFileMetadata(AdditionalText text) =>
+        new FileMetadata
+        {
+            Name = Path.GetFileNameWithoutExtension(text.Path),
+            Extension = Path.GetExtension(text.Path),
+            Contents = text.GetText().ToString()
+        };
 
 }
